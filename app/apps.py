5f647f8a7982370ef3098dd55c2c2238f9f8ad79
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-05-30 00:15:48 -0400

    qwewqe

--- app/apps.py
+++ app/apps.py
@@ -2,9 +2,9 @@ import argparse
 import logging
 import signal
 
-from .camera import make_camera
-from .gstreamer import Display, run_gen
-from .streaming.server import StreamingServer
+from camera import make_camera
+from gstreamer import Display, run_gen
+from streaming.server import StreamingServer
 
 from . import svg
 
