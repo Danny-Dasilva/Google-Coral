5f190ba6ab066d467c7bad21ee4f38e376966243
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-05-31 10:15:55 -0400

    2

--- app/gstreamer.py
+++ app/gstreamer.py
@@ -249,7 +249,10 @@ def on_new_sample(sink, pipeline, render_overlay, layout, images, get_command):
 
         if save_frame:
             images.put((data, layout.inference_size, svg))
-        
+        elif save_frame_1:
+            images.put((data, layout.inference_size, svg))
+        elif save_frame_2:
+            images.put((data, layout.inference_size, svg))
 
     return Gst.FlowReturn.OK
 
