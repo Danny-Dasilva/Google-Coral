04d7f34eb73b056cc4fe1b33fd676a008090b723
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-05-31 00:27:07 -0400

    sepparate thread

--- app/app.py
+++ app/app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from apps import run_server
+from classify import add_render_gen_args, render_gen
+import thread
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, World!"
-    
+
+def main():
+    run_server(add_render_gen_args, render_gen)
+
 if __name__ == "__main__":
+    main
+    thread.start_new_thread(main,())
     app.run(host='0.0.0.0', debug=True)
