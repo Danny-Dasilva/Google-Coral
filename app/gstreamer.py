5f8251e30005322a03151de95b8e978e1f9c1f62
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-05-31 11:11:43 -0400

    changed

--- app/gstreamer.py
+++ app/gstreamer.py
@@ -115,7 +115,7 @@ def Worker(process, maxsize=0):
 
 def save_frame(red ,rgb, size, overlay=None, ext='png'):
 
-    print(str(red, 'utf-8'))
+    print(str( red )[2:-1]
     tag = '%010d' % int(time.monotonic() * 1000)
     img = Image.frombytes('RGB', size, rgb, 'raw')
     #img_pth + 
