4dbf60019b258cbd29ae074f3c80f97857a2e8c1
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-05-31 10:42:45 -0400

    changed path

--- app/gstreamer.py
+++ app/gstreamer.py
@@ -110,7 +110,7 @@ def Worker(process, maxsize=0):
         commands.put(None)
         thread.join()
 
-def save_frame(img_pth, rgb, size, overlay=None, ext='png'):
+def save_frame(rgb, size, overlay=None, ext='png', img_pth):
     tag = '%010d' % int(time.monotonic() * 1000)
     img = Image.frombytes('RGB', size, rgb, 'raw')
     name = img_pth + 'img-%s.%s' % (tag, ext)
